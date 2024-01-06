c9cfc3e32be7296483dcf45de8f75eab5521f3a1
SOHELDESAI <soheldesai000@gmail.com>
2024-01-06 18:44:53 +0530

    Add project files.

---
+++ Controllers/HistogramController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YourNamespace.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HistogramController : Controller
+{
+    private List<DataPoint> GenerateRandomData(int count)
+    {
+        var random = new Random();
+        return Enumerable.Range(1, count)
+            .Select(x => new DataPoint { X = x, Y = random.Next(1, 10) })
+            .ToList();
+    }
+
+    public IActionResult Index()
+    {
+        var histogramData = new List<DataPoint>(GenerateRandomData(10));
+
+        return View(histogramData);
+    }
+
+    [HttpGet]
+    public IActionResult RefreshData()
+    {
+        var refreshedData = GenerateRandomData(10); // Modify this to fetch updated data
+        return Json(refreshedData);
+    }
+}
---
+++ Models/DataPoint.cs
@@ -0,0 +1,8 @@
+﻿namespace YourNamespace.Models
+{
+    public class DataPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
